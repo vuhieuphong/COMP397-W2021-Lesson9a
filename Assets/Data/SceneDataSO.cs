213b21d60e7678e52f53126b1da5b069614e3952
vuhieuphong <52172193+vuhieuphong@users.noreply.github.com>
2021-03-20 03:27:55 -0400

    save load rotation

--- Assets/Data/SceneDataSO.cs
+++ Assets/Data/SceneDataSO.cs
@@ -8,5 +8,6 @@ public class SceneDataSO : ScriptableObject
     //Player Data
     [Header("Player Data")]
     public Vector3 playerPosition;
+    public Quaternion playerRotation;
     public int playerHealth;
 }
--- Assets/_Scripts/ControlPanelController.cs
+++ Assets/_Scripts/ControlPanelController.cs
@@ -106,6 +106,7 @@ public class ControlPanelController : MonoBehaviour
     {
         player.controller.enabled = false;
         player.transform.position= sceneData.playerPosition;
+        player.transform.rotation = sceneData.playerRotation;
         player.controller.enabled = true;
 
         player.health = sceneData.playerHealth;
@@ -115,6 +116,7 @@ public class ControlPanelController : MonoBehaviour
     public void OnSaveButtonPressed()
     {
         sceneData.playerPosition = player.transform.position;
+        sceneData.playerRotation = player.transform.rotation;
         sceneData.playerHealth = player.health;
     }
 }
